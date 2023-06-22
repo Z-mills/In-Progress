24e2bd7aeb99c22961c469f233438ea69581322e
Z-mills <34149125+Z-mills@users.noreply.github.com>
2023-06-22 11:08:26 -0600

    Add files via upload

---
+++ DRLogs.ps1
@@ -0,0 +1,17 @@
+﻿# Set the path for the DR folder
+$DRFolder = "$env:USERPROFILE\Desktop\DR"
+
+# Create the DR folder if it doesn't exist
+if (!(Test-Path $DRFolder)) {
+    New-Item -ItemType Directory -Path $DRFolder | Out-Null
+}
+
+# Copy the logs to the DR folder
+Copy-Item -Path "C:\Windows\System32\winevt\Logs\Application.evtx" -Destination "$DRFolder\Application.evtx" -Force
+Copy-Item -Path "C:\Windows\System32\winevt\Logs\Security.evtx" -Destination "$DRFolder\Security.evtx" -Force
+Copy-Item -Path "C:\Windows\System32\winevt\Logs\System.evtx" -Destination "$DRFolder\System.evtx" -Force
+Copy-Item -Path "C:\Windows\panther\setupact.log" -Destination "$DRFolder\setupact.log" -Force
+Copy-Item -Path "C:\Windows\panther\setuperr.log" -Destination "$DRFolder\setuperr.log" -Force
+
+# Notify the user that the logs have been copied
+Write-Host "Logs copied to $DRFolder"
---
+++ WindowsSecurity.ps1
@@ -0,0 +1,31 @@
+﻿# Check if firewall is enabled
+$fw = Get-NetFirewallProfile -All | Where-Object { $_.Enabled -eq "True" }
+if ($fw) {
+    Write-Host "Firewall is enabled"
+} else {
+    Write-Host "Firewall is not enabled"
+}
+
+# Check if antivirus is installed
+$av = Get-WmiObject -Class "Win32_Product" | Where-Object { $_.Name -like "*antivirus*" }
+if ($av) {
+    Write-Host "Antivirus is installed"
+} else {
+    Write-Host "Antivirus is not installed"
+}
+
+# Check if Windows Defender is enabled
+$defender = Get-MpPreference | Where-Object { $_.RealTimeMonitoringEnabled -eq "True" }
+if ($defender) {
+    Write-Host "Windows Defender is enabled"
+} else {
+    Write-Host "Windows Defender is not enabled"
+}
+
+# Check if User Account Control is enabled
+$uac = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" | Where-Object { $_.EnableLUA -eq "1" }
+if ($uac) {
+    Write-Host "User Account Control is enabled"
+} else {
+    Write-Host "User Account Control is not enabled"
+}
